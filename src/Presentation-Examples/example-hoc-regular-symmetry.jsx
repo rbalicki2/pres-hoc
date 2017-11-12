5a7cd9ac4edabce8a0940ebd2ca719e780399298
Robert <robert.balicki@gmail.com>
2017-11-12 17:33:45 -0500

    fix examples slightly

--- src/Presentation-Examples/example-hoc-regular-symmetry.jsx
+++ src/Presentation-Examples/example-hoc-regular-symmetry.jsx
@@ -3,15 +3,21 @@ import React from 'react';
 
 // higher order version
 const addProp = Component => (props) =>
-  <div><Component {...props} meaningOfLife="42" /></div>
+  <Component {...props} meaningOfLife={42} />
 
 // traditional version
 const AddProp = (props) =>
-  <div>
-    {
-      React.cloneElement(this.props.children, {
-        meaningOfLife: 42
-      })
-    }
-  </div>;
+  React.cloneElement(this.props.children, {
+    meaningOfLife: 42
+  });
+
+// Render prop
+const AddPropWithRenderProp = (props) => this.props.children(42);
+
+// use it like:
+<AddPropWithRenderProp>
+  { meaningOfLife => <MyExistingComponent meaningOfLife={meaningOfLife} /> }
+</AddPropWithRenderProp>
+
+// NOTE: children is a function. That looks weird, but get over it!
 
