477b3b1277f7a0a96280542903663191494fcfe6
Robert <robert.balicki@gmail.com>
2017-11-12 19:05:59 -0500

    V3 - WindowSizeScrollProvider
    - make it work, and add a helpful console.log for demo purposes

--- src/components/WindowSizeScrollProvider.jsx
+++ src/components/WindowSizeScrollProvider.jsx
@@ -1,8 +1,31 @@
 import { Component } from 'react';
+import debounce from 'lodash/debounce';
+
+const SCROLL_RESIZE_DEBOUNCE_MS = 300;
 
 export default class WindowSizeScrollProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.handleScrollOrResize = debounce(this.handleScrollOrResize.bind(this), SCROLL_RESIZE_DEBOUNCE_MS);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScrollOrResize);
+    window.addEventListener('resize', this.handleScrollOrResize);
+  }
+
+  handleScrollOrResize() {
+    this.setState({
+      scrollX: window.scrollX,
+      scrollY: window.scrollY,
+      innerHeight: window.innerHeight,
+      innerWidth: window.innerWidth,
+    }, () => console.log(this.state));
+  }
+
   render() {
     return this.props.children({
+      w: this.state,
     });
   }
 }
